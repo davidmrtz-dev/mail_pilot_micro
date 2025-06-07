bcaf9a558892791c132e0a5ee6ab068a4f9c0a36
davidmrtz-dev <davidelimg1@gmail.com>
2025-06-07 02:06:46 -0600

    [WIP]: Integrated oled feature

--- mp_oled.ino
+++ mp_oled.ino
@@ -4,29 +4,28 @@
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
+#define TEXT_BUFFER_SIZE 64
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
+char currentText[TEXT_BUFFER_SIZE] = "Esperando texto...";
 int scrollX = SCREEN_WIDTH;
 
-void setup() {
-  Serial.begin(115200);
-
+void setupDisplay() {
   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println(F("SSD1306 allocation failed"));
-    for (;;);
+    while (true);
   }
 
-  display.setTextSize(2);
+  display.setTextSize(4);
   display.setTextColor(WHITE);
   display.setTextWrap(false);
 }
 
-void loop() {
+void scrollText(const char* text) {
   display.clearDisplay();
 
-  const char* text = "Scrolling Hello";
-  int y = (SCREEN_HEIGHT - 16) / 2;
+  int y = (SCREEN_HEIGHT - 32) / 2;
   display.setCursor(scrollX, y);
   display.print(text);
   display.display();
@@ -37,6 +36,26 @@ void loop() {
   if (scrollX < -textWidth) {
     scrollX = SCREEN_WIDTH;
   }
+}
 
-  delay(20);
+void checkSerialInput() {
+  if (Serial.available()) {
+    String input = Serial.readStringUntil('\n');
+    input.trim();
+    if (input.length() > 0 && input.length() < TEXT_BUFFER_SIZE) {
+      input.toCharArray(currentText, TEXT_BUFFER_SIZE);
+      scrollX = SCREEN_WIDTH; // Reinicia el scroll
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  setupDisplay();
+}
+
+void loop() {
+  checkSerialInput();
+  scrollText(currentText);
+  delay(2);
 }
